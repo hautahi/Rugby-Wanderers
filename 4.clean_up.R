27fad02df56bf7ca2790a2c2b3e99464bd39487b
hautahi <hrk55@cornell.edu>
2015-09-11 17:00:16 -0400

    Further manual adjustments

--- 4.clean_up.R
+++ 4.clean_up.R
@@ -1,5 +1,6 @@
-# This program ...... 
+# This program "cleans" the resulting dataset from the Python files.
 library(data.table)
+library(plyr)
 
 # Load data created from Python
 d <- read.csv("complete.csv", header=FALSE)
@@ -9,68 +10,112 @@ oldnames = c('V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13
 newnames = c('player','debut','position','tests','wins','losses','draws','points','tries','birthplace','country1','country2','country3','country4','team')
 setnames(d, old = oldnames, new = newnames)
 
-# Unknown countries are returned as "g". Make these countries blank.
+#-------------------------------------------------------------#
+# Remove known errors
+#-------------------------------------------------------------#
+
+# Unknown countries returned as "g". Make these countries blank.
 d$country2 <- as.character(d$country2)
-d$country2[d$country1 == 'g'] <- ""
-
 d$country3 <- as.character(d$country3)
-d$country3[d$country1 == 'g'] <- ""
-
+d$country4 <- as.character(d$country4)
 d$country1 <- as.character(d$country1)
+
+d$country2[d$country1 == 'g'] <- ""
+d$country3[d$country1 == 'g'] <- ""
+d$country4[d$country1 == 'g'] <- ""
 d$country1[d$country1 == 'g'] <- ""
 
-# Create a new variable that combines all country info.
-d$han <- paste(d$country1,d$country2,sep="")
-d$han <- paste(d$han,d$country3,sep="")
-d$han <- paste(d$han,d$country4,sep="")
+# Clear known errors in birthplace
+d$birthplace[d$birthplace == 'date unknown'] <- ""
+d$birthplace[d$birthplace == '?'] <- ""
+
+d$country1[grepl(pattern="[[:digit:]]", x=d$birthplace)==TRUE] <- ""
+d$country2[grepl(pattern="[[:digit:]]", x=d$birthplace)==TRUE] <- ""
+d$country3[grepl(pattern="[[:digit:]]", x=d$birthplace)==TRUE] <- ""
+d$birthplace[grepl(pattern="[[:digit:]]", x=d$birthplace)==TRUE]=""
+
+d$born[grepl(pattern="[[:digit:]]", x=d$birthplace)==TRUE]=""
 
 #-------------------------------------------------------------#
-# Manual Adjustments.
+# Streamline country names
 #-------------------------------------------------------------#
 
-# Using country manual adjustment file, assign country.
+# Load country manual adjustment file and create dictionary
 dic <- read.csv("manual_adjustments/country_codes.csv",header=F)
-d1 <- as.character(dic$V1)
 COUNTRY <- as.character(dic$V2)
-names(COUNTRY) <- d1
+names(COUNTRY) <- as.character(dic$V1)
+
+# Create a new variable that combines all country info
+d$han <- paste(d$country1,d$country2,d$country3,d$country4,sep=" ")
 
 # Search for each of the country names within the big geography string, then assign
 d$born <- d$country1
 for (i in 1:length(COUNTRY)){
-odie <- grepl(names(COUNTRY[i]),d$han)
-d$born[odie==TRUE]=COUNTRY[[i]]
+d$born[grepl(names(COUNTRY[i]),d$han)==TRUE]=COUNTRY[[i]]
 }
 
 # The remaining errors are now a result of incorrect geocoding in Python.
-# The remainder of this code manually corrects some entries, first based on known cities, then players.
 
-# Using city manual adjustment file, assign country.
+#-------------------------------------------------------------#
+# Manually assign countries according to city name
+#-------------------------------------------------------------#
+
+# Load city manual adjustment file and create dictionary
 dic <- read.csv("manual_adjustments/city_codes.csv",header=F)
-d1 <- as.character(dic$V1)
 CITY <- as.character(dic$V2)
-names(CITY) <- d1
+names(CITY) <- as.character(dic$V1)
 
-# Search for each of the city names within the big geography string, then assign
+# Search for each of the city names within the birthplace variable, then assign country accordingly
 for (i in 1:length(CITY)){
 odie <- grepl(names(CITY[i]),d$birthplace)
 d$born[odie==TRUE]=CITY[[i]]
 }
 
-# Using player manual adjustment file, assign country.
+#-------------------------------------------------------------#
+# Manually assign details according to player name
+#-------------------------------------------------------------#
+
+# Load player manual adjustment file and name variables
+dic <- read.csv("manual_adjustments/player_codes.csv",header=F)
+d1 <- as.character(dic$V1)
+PLAYER <- as.character(dic$V2)
+PLAYER_CITY <- as.character(dic$V3)
+names(PLAYER) <- d1
+
 d$player <- as.character(d$player)
 d$birthplace <- as.character(d$birthplace)
 
-dic <- read.csv("manual_adjustments/player_codes1.csv",header=F)
-d1 <- as.character(dic$V1)
-PLAYER <- as.character(dic$V2)
-d3 <- as.character(dic$V3)
-names(PLAYER) <- d1
-
-# Search for each of the player names within the name string, then assign
+# Search for each of the player names (exact match) within the name string, then assign country and maybe city
 for (i in 1:length(PLAYER)){
-odie <- grepl(names(PLAYER[i]),d$player)
-d$born[odie==TRUE]=PLAYER[[i]]
-d$birthplace[odie==TRUE]=PLAYER_CITY[i]
+  d$born[d$player==names(PLAYER[i])]=PLAYER[[i]]
+  d$birthplace[d$player==names(PLAYER[i])]=PLAYER_CITY[i]
+}
+
+#-------------------------------------------------------------#
+# Manually assign details from NZ Herald Data
+#-------------------------------------------------------------#
+# The NZ Herald compiled a comprehensive list. This section exploits
+# that list
+
+NZ_Herald <- read.csv("manual_adjustments/NZ_data.csv",header=T)
+NZ_Manual <- read.csv("manual_adjustments/NZ_data_manual.csv",header=F)
+
+NZ_Manual$Number <- NZ_Manual$V2
+
+plyr1 <- join(NZ_Manual, NZ_Herald, by = "Number")
+keepvars <- c("V1","Place","Number")
+NZ_index <- plyr1[keepvars]
+
+names(NZ_index)[names(NZ_index)=="V1"] <- "player"
+NZ_index$country <- sub('.*,\\s*', '', NZ_index$Place)
+NZ_index$cities <- gsub("^(.*?),.*", "\\1", NZ_index$Place)
+
+d <- join(d, NZ_index, by = "player")
+d$born[!is.na(d$Number)] <- d$country[!is.na(d$Number)]
+d$birthplace[!is.na(d$Number)] <- d$cities[!is.na(d$Number)]
+
+for (i in 1:length(COUNTRY)){
+  d$born[grepl(names(COUNTRY[i]),d$born)==TRUE]=COUNTRY[[i]]
 }
 
 #-------------------------------------------------------------#
@@ -78,9 +123,14 @@ d$birthplace[odie==TRUE]=PLAYER_CITY[i]
 #-------------------------------------------------------------#
 
 new_data <- data.frame("player" = d$player, "team" = d$team, "debut" = d$debut,
-                       "tests"=d$tests,"win"=d$wins,"loss"=d$losses, "points"=d$points,"tries"=d$tries,
-                       "birthplace" = d$birthplace, "country"=d$born) 
+                       "tests"=d$tests,"win"=d$wins,"loss"=d$losses, "draw"=d$draws,"points"=d$points,
+                       "tries"=d$tries, "birthplace" = d$birthplace, "country"=d$born) 
 write.csv(new_data, "final_data.csv", row.names=TRUE)
 
+#NZ_data <- read.csv("NZ_data.csv",header=T)
+#NZ_data <- NZ_data[!(NZ_data$Tests==0),]
+#NZ_data$Place <- as.character(NZ_data$Place)
+#NZ_data$Name <- as.character(NZ_data$Name)
 
-
+#read.table(NZ_data$Place, sep = ",", colClasses = "character")
+#x <- strsplit(NZ_data$Place, ", ")
